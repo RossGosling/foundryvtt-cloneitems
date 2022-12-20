7e12cc0e552d640015a5a79e97bd1ca835eecaf9
Ross Gosling <ross.gosling@btinternet.com>
2022-12-20 03:46:31 +0000

    implement on-Sheet-Load handling

---
+++ src/module.js
@@ -0,0 +1,49 @@
+
+Hooks.once("setup", async function () {
+
+    if (game.system.id === "dnd5e") {
+        Hooks.on("renderActorSheet5e", renderInjectionHook);
+        Hooks.on("renderActorSheet5eNew", renderInjectionHook);
+    }
+
+    console.log("CloneItems | Loaded");
+});
+
+async function renderInjectionHook(sheet, element, character) {
+    try {
+
+        const actor = game.actors.get(character.actor._id);
+
+        const itemElements = $(".items-list.inventory-list>.item-list>.item", element);
+        for (const itemElement of itemElements) {
+            for (const itemControlsElement of $(".item-controls", itemElement)) {
+
+                const item = actor.items.get(itemElement.dataset?.itemId);
+                if (item && !["race", "background", "class", "subclass"].includes(item.type)) {
+
+                    const cloneButton = $(`<a class="item-control item-clone" title="${ game.i18n.localize("CloneItems.ButtonTitle") }"><i class="fas fa-clone"></i></a>`)[0];
+
+                    cloneButton.addEventListener(
+                        "click",
+                        async() => {
+                            try {
+                                return actor.createEmbeddedDocuments("Item", [item])
+                            } catch(error) {
+                                console.error("CloneItems | https://github.com/RossGosling/foundryvtt-cloneitems/issues\nError during button press\n", error);
+                            }
+                        }
+                    );
+
+                    itemControlsElement.insertBefore(
+                        cloneButton,
+                        Array.from(itemControlsElement.children)
+                            .find((itemControl) => itemControl.classList.contains('item-delete'))
+                    );
+                }
+            }
+        }
+
+    } catch(error) {
+        console.error("CloneItems | https://github.com/RossGosling/foundryvtt-cloneitems/issues\nError during sheet load\n", error);
+    }
+}
